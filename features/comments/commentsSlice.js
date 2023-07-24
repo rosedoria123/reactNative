a86fca7e958de242f83a4df373f8623c19285b4b
rosedoria123 <113267533+rosedoria123@users.noreply.github.com>
2023-07-24 10:19:03 -0500

    Add files via upload

---
+++ features/comments/commentsSlice.js
@@ -0,0 +1,62 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { baseUrl } from '../../shared/baseUrl';
+
+export const postComment = createAsyncThunk(
+    'comments/postComment',
+    async (payload, { dispatch, getState }) => {
+      setTimeout(() => {
+        const { comments } = getState();
+        const currentDate = new Date().toISOString();
+        const id = comments.commentsArray.length;
+  
+        payload.date = currentDate;
+        payload.id = id;
+  
+        dispatch(addComment(payload)); 
+      }, 2000);
+    }
+);
+  
+export const fetchComments = createAsyncThunk(
+    'comments/fetchComments',
+    async () => {
+        const response = await fetch(baseUrl + 'comments');
+        if (!response.ok) {
+            return Promise.reject(
+                'Unable to fetch, status: ' + response.status
+            );
+        }
+        const data = await response.json();
+        return data;
+    }
+);
+
+const commentsSlice = createSlice({
+    name: 'comments',
+    initialState: { isLoading: true, errMess: null, commentsArray: [] },
+    reducers: {
+        addComment: (state, action) => {
+            state.commentsArray.push(action.payload);      
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchComments.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(fetchComments.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.errMess = null;
+                state.commentsArray = action.payload;
+            })
+            .addCase(fetchComments.rejected, (state, action) => {
+                state.isLoading = false;
+                state.errMess = action.error
+                    ? action.error.message
+                    : 'Fetch failed';
+            });
+    }
+});
+
+export const { addComment } = commentsSlice.actions;
+export const commentsReducer = commentsSlice.reducer;
